46bb82af1149272a286c3a7a1a3c3a59479da4b2
Jerome Gordon-Bock <jerome.bock@gmail.com>
2026-02-27 10:07:22 +0100

    Initial + updated migration

--- Golfklubb-Centar-Webbshop/Areas/Identity/Data/ApplicationDbContext.cs
+++ Golfklubb-Centar-Webbshop/Areas/Identity/Data/ApplicationDbContext.cs
@@ -18,5 +18,41 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+        RenameIdentityTables(builder);
+    }
+
+    protected void RenameIdentityTables(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.HasDefaultSchema("CentarUserMngt");
+        builder.Entity<ApplicationUser>(entity =>
+        {
+            entity.ToTable(name: "Users");
+        });
+        builder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable(name: "Roles");
+        });
+        builder.Entity<IdentityUserRole<string>>(entity =>
+        {
+            entity.ToTable(name: "UserRoles");
+        });
+        builder.Entity<IdentityUserClaim<string>>(entity =>
+        {
+            entity.ToTable(name: "UserClaims");
+        });
+        builder.Entity<IdentityUserLogin<string>>(entity =>
+        {
+            entity.ToTable(name: "UserLogin");
+        });
+        builder.Entity<IdentityRoleClaim<string>>(entity =>
+        {
+            entity.ToTable(name: "RoleClaims");
+        });
+        builder.Entity<IdentityUserToken<string>>(entity =>
+        {
+            entity.ToTable(name: "UserTokens");
+        });
+
     }
 }
