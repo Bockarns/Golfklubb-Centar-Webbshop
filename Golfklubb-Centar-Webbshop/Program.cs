cc152595d62afc54e6d6977f250291c2c8aa250c
Jerome Gordon-Bock <jerome.bock@gmail.com>
2026-03-09 10:08:19 +0100

    Added SeedData (Admin,User, Products,Discount,Category)

--- Golfklubb-Centar-Webbshop/Program.cs
+++ Golfklubb-Centar-Webbshop/Program.cs
@@ -6,12 +6,25 @@ var connectionString = builder.Configuration.GetConnectionString("ApplicationDbC
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                                                    .AddRoles<IdentityRole>()
+                                                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                                                    .AddDefaultTokenProviders()
+                                                    .AddDefaultUI();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    await SeedData.SeedRoles(scope.ServiceProvider);
+    await SeedData.SeedDiscount(scope.ServiceProvider.GetRequiredService<ApplicationDbContext>());
+    await SeedData.SeedCategory(scope.ServiceProvider.GetRequiredService<ApplicationDbContext>());
+    await SeedData.SeedProduct(scope.ServiceProvider.GetRequiredService<ApplicationDbContext>());
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -32,6 +45,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
+
+app.MapRazorPages();
 
 
 app.Run();
