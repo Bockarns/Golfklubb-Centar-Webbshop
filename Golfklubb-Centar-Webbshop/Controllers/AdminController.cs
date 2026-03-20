fdddff332c938440f63d91eccf222c64e4b16c91
Ninos Pito <Ninospito@hotmail.com>
2026-03-20 11:48:32 +0100

    admin panel + controller

---
+++ Golfklubb-Centar-Webbshop/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using Golfklubb_Centar_Webbshop.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Golfklubb_Centar_Webbshop.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+
+        private readonly UserManager<IdentityUser> _userManager;
+
+        private readonly ApplicationDbContext _context;
+        public AdminController(ILogger<AdminController> logger, UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _userManager = userManager;
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _userManager.Users.ToListAsync();
+
+            return View(users);
+        }
+    }
+}
