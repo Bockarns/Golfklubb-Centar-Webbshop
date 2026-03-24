d85b2ca1db38971fa7cf8383b906a7d09a0f0f0a
Jerome Gordon-Bock <jerome.bock@gmail.com>
2026-03-24 20:48:21 +0100

    ValidateNever on virtual

--- Golfklubb-Centar-Webbshop/Models/Product.cs
+++ Golfklubb-Centar-Webbshop/Models/Product.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace Golfklubb_Centar_Webbshop.Models;
 
@@ -40,10 +41,12 @@ public partial class Product
 
     [ForeignKey("FkCategoryId")]
     [InverseProperty("Products")]
+    [ValidateNever] //För att kunna spara produkt behövde jag lägga till denna + den nedanför för att ignora objektet och endast Id kan läggas till
     public virtual Category FkCategory { get; set; } = null!;
 
     [ForeignKey("FkDiscountId")]
     [InverseProperty("Products")]
+    [ValidateNever] //För att kunna spara produkt behövde jag lägga till denna för att ignora objektet och endast Id kan läggas till
     public virtual Discount FkDiscount { get; set; } = null!;
 
     [InverseProperty("FkProduct")]
