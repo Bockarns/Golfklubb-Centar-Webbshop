bb219ef994a072327f6bda0d62597a1182be49d8
Ninos Pito <Ninospito@hotmail.com>
2026-03-26 15:59:36 +0100

    added CategoryFilter + sidebar category-nav

---
+++ Golfklubb-Centar-Webbshop/Models/CategoryFilterViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Golfklubb_Centar_Webbshop.Models
+{
+    public class CategoryFilterViewModel
+    {
+        public List<Product> Products { get; set; } = new();
+        public List<Category> ParentCategories { get; set; } = new();
+        public int? SelectedCategory { get; set; }
+    }
+}
