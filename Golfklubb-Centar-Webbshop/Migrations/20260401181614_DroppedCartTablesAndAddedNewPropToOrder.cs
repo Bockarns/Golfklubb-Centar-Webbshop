dbfc763548ab910355df7bd9d431c5ff88133799
Ninos Pito <Ninospito@hotmail.com>
2026-04-01 20:22:47 +0200

    Dropped Cart Tables And Added New Prop To Order

---
+++ Golfklubb-Centar-Webbshop/Migrations/20260401181614_DroppedCartTablesAndAddedNewPropToOrder.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Golfklubb_Centar_Webbshop.Migrations
+{
+    /// <inheritdoc />
+    public partial class DroppedCartTablesAndAddedNewPropToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_CartId",
+                schema: "CentarOrderMngt",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "CartItems",
+                schema: "CentarOrderMngt");
+
+            migrationBuilder.DropTable(
+                name: "Carts",
+                schema: "CentarOrderMngt");
+
+            //migrationBuilder.DropIndex(
+            //    name: "IX_Orders_FK_CartId",
+            //    schema: "CentarOrderMngt",
+            //    table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "FK_CartId",
+                schema: "CentarOrderMngt",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                schema: "CentarOrderMngt",
+                table: "Orders",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                schema: "CentarOrderMngt",
+                table: "Orders",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                schema: "CentarOrderMngt",
+                table: "Orders",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                schema: "CentarOrderMngt",
+                table: "Orders",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                schema: "CentarOrderMngt",
+                table: "Orders",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                schema: "CentarOrderMngt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                schema: "CentarOrderMngt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                schema: "CentarOrderMngt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                schema: "CentarOrderMngt",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                schema: "CentarOrderMngt",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "FK_CartId",
+                schema: "CentarOrderMngt",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                schema: "CentarOrderMngt",
+                columns: table => new
+                {
+                    CartId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TotalPrice = table.Column<decimal>(type: "decimal(7,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartId", x => x.CartId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartItems",
+                schema: "CentarOrderMngt",
+                columns: table => new
+                {
+                    CartItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FK_CartId = table.Column<int>(type: "int", nullable: false),
+                    FK_ProductId = table.Column<int>(type: "int", nullable: false),
+                    AddedDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItemId", x => x.CartItemId);
+                    table.ForeignKey(
+                        name: "FK_CartItems_CartId",
+                        column: x => x.FK_CartId,
+                        principalSchema: "CentarOrderMngt",
+                        principalTable: "Carts",
+                        principalColumn: "CartId");
+                    table.ForeignKey(
+                        name: "FK_CartItems_ProductId",
+                        column: x => x.FK_ProductId,
+                        principalSchema: "CentarProductMngt",
+                        principalTable: "Products",
+                        principalColumn: "ProductId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_FK_CartId",
+                schema: "CentarOrderMngt",
+                table: "Orders",
+                column: "FK_CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_FK_CartId",
+                schema: "CentarOrderMngt",
+                table: "CartItems",
+                column: "FK_CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItems_FK_ProductId",
+                schema: "CentarOrderMngt",
+                table: "CartItems",
+                column: "FK_ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_CartId",
+                schema: "CentarOrderMngt",
+                table: "Orders",
+                column: "FK_CartId",
+                principalSchema: "CentarOrderMngt",
+                principalTable: "Carts",
+                principalColumn: "CartId");
+        }
+    }
+}
--- Golfklubb-Centar-Webbshop/Models/Order.cs
+++ Golfklubb-Centar-Webbshop/Models/Order.cs
@@ -13,9 +13,6 @@ public partial class Order
     [Key]
     public int OrderId { get; set; }
 
-    [Column("FK_CartId")]
-    public int FkCartId { get; set; }
-
     [Column("FK_UserId")]
     [StringLength(450)]
     public string FkUserId { get; set; } = null!;
@@ -24,9 +21,20 @@ public partial class Order
     [Unicode(false)]
     public string? OrderStatus { get; set; }
 
-    [ForeignKey("FkCartId")]
-    [InverseProperty("Orders")]
-    public virtual Cart FkCart { get; set; } = null!;
+    [StringLength(50)]
+    public string Address { get; set; } = null!;
+
+    [StringLength(10)]
+    public string PostalCode { get; set; } = null!;
+
+    [StringLength(20)]
+    public string City { get; set; } = null!;
+
+    [StringLength(50)]
+    public string Country { get; set; } = null!;
+
+    [StringLength(20)]
+    public string Phone { get; set; } = null!;
 
     [ForeignKey("FkUserId")]
     [InverseProperty("Orders")]
