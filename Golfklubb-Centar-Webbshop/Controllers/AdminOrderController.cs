7208342b35c4cf6da03d0d3aebd9da97d2a96853
Jerome Gordon-Bock <jerome.bock@gmail.com>
2026-04-07 13:51:17 +0200

    Admin Order Controller + cshtml

---
+++ Golfklubb-Centar-Webbshop/Controllers/AdminOrderController.cs
@@ -0,0 +1,24 @@
+﻿using Golfklubb_Centar_Webbshop.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Golfklubb_Centar_Webbshop.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminOrderController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public AdminOrderController(ILogger<AdminController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Orders()
+        {
+            return View();
+        }
+    }
+}
