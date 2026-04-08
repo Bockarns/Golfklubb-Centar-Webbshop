b924b5526a93654c3652453e9df91e955cd78bfd
Jerome Gordon-Bock <jerome.bock@gmail.com>
2026-04-08 11:41:23 +0200

    Admin User order and forum history, working on updating useredit and user details with resent changes from Peyman

---
+++ Golfklubb-Centar-Webbshop/Models/UserForumHistoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Golfklubb_Centar_Webbshop.Models
+{
+    public class UserForumHistoryViewModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public List<Post> Posts { get; set; } = new();
+        public List<Comment> Comments { get; set; } = new();
+    }
+}
